81da01d94885d133549aedddb4211a5adc09180d
Mike <qazwqsx9@gmail.com>
2020-02-03 13:38:08 -0500

    change to cpp

---
+++ raspberry/wiringMain.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include "serialHandler.h"
+int main (void)
+{
+  printf ("Raspberry Pi - Gertboard Blink\n") ;
+  wiringPiSetup () ;
+  SerialHandler sh;
+  Datapacket dp=new Datapacket;
+  dp->data="test";
+  dp->length=4;
+  
+  while (1)
+  {
+    sh << dp;
+  }
+  
+  return 0 ;
+}
