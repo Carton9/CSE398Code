c4b1e763da5bad0ea30a52065034dc6685459dfd
Mike <qazwqsx9@gmail.com>
2020-02-17 14:18:34 -0500

    debug

---
+++ raspberry_3/wiringMain.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <cstdlib>
+#include <iostream> 
+#include <fstream>
+#include <wiringPi.h>
+#include <wiringSerial.h>
+#include <wiringPiI2C.h>
+#include <unistd.h>
+union FloatArray
+{
+  float array[10];
+  char data[sizeof(float)*10];
+};
+int main (void)
+{
+  printf ("Raspberry Pi - Gertboard Blink\n") ;
+  wiringPiSetup () ;
+  int fd=routingPiI2CSetup£¨0x70);
+  cout << fd;
+  return 0 ;
+}
+
+
+
+
+    // for (int i = 0; i < dp2->length; i++)
+    // {
+    //     cout << hex << (uint)dp2->data[i] << " ";
+    // }
+    // cout << endl;
+    // if (dp2->length<1)continue;
+    
+    // // cout << dp2->length ;
+    // cout << "Value" << (cod<<dp2) << endl;
+
+        // sh << dp;
+    // cout  << "DP1" << dp->data << endl;
+
+    // sh >>dp2;
+    // if (dp2->length<1)continue;
+    // cod>=dp2;
+    // FloatArray fa;
+    // memcpy(fa.data,dp2->data,40);
+    // for (int i = 0; i < 10; i++)
+    // {
+    //   cout << fa.array[i] << " ";
+    // }
+    // cout << endl;
+    
+
+
+    // usleep(100000);
