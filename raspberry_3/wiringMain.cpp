411a3fc330535561ab5104bcd25ad16502c6abba
Mike <qazwqsx9@gmail.com>
2020-02-17 15:05:47 -0500

    debug

--- raspberry_3/wiringMain.cpp
+++ raspberry_3/wiringMain.cpp
@@ -8,10 +8,8 @@
 #include <unistd.h>
 #include "Adafruit_PWMServoDriver.h"
 using namespace std;
-#define SERVOMIN  200 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMIN  500 // This is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  1000 // This is the 'maximum' pulse length count (out of 4096)
-#define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
-#define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
 union FloatArray
 {
