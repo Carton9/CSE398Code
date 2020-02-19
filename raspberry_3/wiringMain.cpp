7d6c4a11ce700ba25a133f32f182266be3b3c918
Mike <qazwqsx9@gmail.com>
2020-02-19 13:35:08 -0500

    debug

--- raspberry_3/wiringMain.cpp
+++ raspberry_3/wiringMain.cpp
@@ -11,40 +11,40 @@ using namespace std;
 #define SERVOMIN  500 // This is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  1000 // This is the 'maximum' pulse length count (out of 4096)
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
-union FloatArray
-{
-  float array[10];
-  char data[sizeof(float)*10];
-};
-int main (void)
-{
-  printf ("Raspberry Pi - Gertboard Blink\n") ;
-  wiringPiSetup () ;
-  // int fd=wiringPiI2CSetup(0x70);
-  Adafruit_PWMServoDriver pwm(0x40);
-  pwm.begin();
-  pwm.setOscillatorFrequency(27000000);
-  pwm.setPWMFreq(SERVO_FREQ);
-  int turn[3]={460,300,130};
-  int rise[3]={420,400,250};
-   while (1)
-   {
-    int value;
-    cin >>value;
-    // for (int i = 0; i < 3; i++)
-    // {
-      pwm.setPWM(0, 0, turn[value]);
-      pwm.setPWM(1, 0, rise[value]);
-      usleep(500*1000);
-    // }
+// union FloatArray
+// {
+//   float array[10];
+//   char data[sizeof(float)*10];
+// };
+// int main (void)
+// {
+//   printf ("Raspberry Pi - Gertboard Blink\n") ;
+//   wiringPiSetup () ;
+//   // int fd=wiringPiI2CSetup(0x70);
+//   Adafruit_PWMServoDriver pwm(0x40);
+//   pwm.begin();
+//   pwm.setOscillatorFrequency(27000000);
+//   pwm.setPWMFreq(SERVO_FREQ);
+//   int turn[3]={460,300,130};
+//   int rise[3]={420,400,250};
+//    while (1)
+//    {
+//     int value;
+//     cin >>value;
+//     // for (int i = 0; i < 3; i++)
+//     // {
+//       pwm.setPWM(0, 0, turn[value]);
+//       pwm.setPWM(1, 0, rise[value]);
+//       usleep(500*1000);
+//     // }
     
     
-   }
+//    }
    
    
-  // cout << fd;
-  return 0 ;
-}
+//   // cout << fd;
+//   return 0 ;
+// }
 
 
 
