ce8e690277de21cd4f4c8fad8085fb3971d36486
Mike <qazwqsx9@gmail.com>
2020-03-09 15:03:14 -0400

    debug

---
+++ raspberry_4/Motor.h
@@ -0,0 +1,35 @@
+#ifndef __MOTOR_H
+#define __MOTOR_H
+#include <wiringPi.h>
+#include <softPwm.h>
+#include <stdio.h>
+#include <cstdlib>
+#include <unistd.h>
+#include "opencv2/opencv.hpp"
+#include <vector>
+#include <thread>
+class Motor{
+    public:
+        Motor(int motorPWM,int motorA,int motorB);{
+            pinMode (motorA, OUTPUT) ;
+            pinMode (motorB, OUTPUT) ;
+            softPwmCreate (motorPWM, 0, 100) ;
+            motors[0]=motorPWM;
+            motors[1]=motorA;
+            motors[2]=motorB;
+        }
+        ~Motor(){
+            softPwmWrite (motors[0], 0);
+            digitalWrite (motors[1], 0) ;
+            digitalWrite (motors[2], 0) ;
+        }
+        void motor_control(int speed){
+            int dir=speed>0;
+            digitalWrite (motors[1], dir) ;
+            digitalWrite (motors[2], !dir) ;
+            softPwmWrite (motors[0], abs(speed));
+        }
+    private:
+        int motors[3];
+}
+#endif
--- raspberry_4/wiringMain.cpp
+++ raspberry_4/wiringMain.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <wiringPi.h>
 #include <softPwm.h>
+#include <Motor.h>
 // #include <wiringSerial.h>
 // #include <wiringPiI2C.h>
 #include <unistd.h>
@@ -14,59 +15,42 @@
 #include <thread>
 using namespace cv;
 using namespace std;
-void motor_control(int motor, int speed){
-  int dir=speed>0;
-  if (motor==0)
-  {
-    digitalWrite (24, dir) ;
-    digitalWrite (25, !dir) ;
-    softPwmWrite (23, abs(speed));
-  }else{
-    digitalWrite (28, dir) ;
-    digitalWrite (27, !dir) ;
-    softPwmWrite (26, abs(speed));
-  }
-}
+// void motor_control(int motor, int speed){
+//   int dir=speed>0;
+//   if (motor==0)
+//   {
+//     digitalWrite (24, dir) ;
+//     digitalWrite (25, !dir) ;
+//     softPwmWrite (23, abs(speed));
+//   }else{
+//     digitalWrite (28, dir) ;
+//     digitalWrite (27, !dir) ;
+//     softPwmWrite (26, abs(speed));
+//   }
+// }
 
 int main(int argc, char** argv)
 {
   wiringPiSetup () ;
-  pinMode (24, OUTPUT) ;
-  pinMode (25, OUTPUT) ;
+  // pinMode (24, OUTPUT) ;
+  // pinMode (25, OUTPUT) ;
 
-  pinMode (28, OUTPUT) ;
-  pinMode (27, OUTPUT) ;
+  // pinMode (28, OUTPUT) ;
+  // pinMode (27, OUTPUT) ;
   
-  softPwmCreate (23, 0, 100) ;
-  softPwmCreate (26, 0, 100) ;
+  // softPwmCreate (23, 0, 100) ;
+  // softPwmCreate (26, 0, 100) ;
+  Motor motor1(23,24,25);
+  Motor motor2(26,28,27);
   while(1){
-    // digitalWrite (24, 0) ;
-    // digitalWrite (25, 1) ;
-
-    // digitalWrite (28, 0) ;
-    // digitalWrite (27, 1) ;
     for (int i = -100; i < 100; i++)
     {
       // softPwmWrite (23, i);
       // softPwmWrite (26, i);
-      motor_control(0,i);
-      motor_control(1,i);
+      motor1.motor_control(i);
+      motor2.motor_control(i);
       usleep(10000);
     }
-
-    // digitalWrite (24, 1) ;
-    // digitalWrite (25, 0) ;
-
-    // // digitalWrite (27, 1) ;
-    // // digitalWrite (22, 0) ;
-    // for (int i = 0; i < 100; i++)
-    // {
-    //   softPwmWrite (23, i);
-    //   // softPwmWrite (17, i);
-    //   usleep(100000);
-    // }
-
-    
   }
   return 0;
 }
